2ecc3f45136c831f581230cea10ecf4904ada313
khomavolha <52779547+khomavolha@users.noreply.github.com>
2020-03-11 09:39:08 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 
 module.exports = function towelSort (matrix) {
     let arr=[];
-    if(matrix == undefined || matrix.length == 0){ return []
+    if(matrix === undefined || matrix.length === 0){ return []
         } else {
             for(let a=1; a<matrix.length; a=a+2){
             matrix[a] = matrix[a].reverse();
