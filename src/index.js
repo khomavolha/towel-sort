ac964d85fc7dbe0db25cedd886463e31cd84538c
khomavolha <52779547+khomavolha@users.noreply.github.com>
2020-03-11 09:53:11 +0300

    feat: endly solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 
 module.exports = function towelSort (matrix) {
-    if(matrix === undefined || matrix.length === 0) {
-        return [];
-    } else {
-        for(let i = 1; i < matrix.length; i=i+2) {
-            matrix[i] = matrix[i].reverse();
-        };
-        let arr = [].concat(...matrix);
-        return arr; 
-    };
+    let arr=[];
+    if(matrix === undefined || matrix.length === 0){ return []
+        } else {
+            for(let a=1; a<matrix.length; a=a+2){
+            matrix[a] = matrix[a].reverse();
+            }
+    arr = matrix.flat(Infinity);
+
+        }
+    return arr;
 }
